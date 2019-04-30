843dc213fa6ac4e6e40462b24fa984990389a427
Shaun Sharples <shaun.sharples@gmail.com>
2019-04-30 14:05:28 +0200

    default.nix takes pkgs as argument

--- default.nix
+++ default.nix
@@ -1,4 +1,29 @@
 let
-  drv = (import ./servant-event-stream.nix).servant-event-stream;
+  sources = import nix/sources.nix;
+  nixpkgs = import sources.nixpkgs {};
 in
-  drv
+
+{ pkgs ? nixpkgs.pkgs }:
+
+let
+  inherit (pkgs.haskell.lib) doJailbreak dontCheck dontHaddock;
+
+  haskellPackages = pkgs.haskellPackages.override {
+    overrides = self: super: {
+    };
+  };
+
+  drv = haskellPackages.callCabal2nix "servant-event-stream" ./. {};
+
+in rec
+{
+  servant-event-stream = drv;
+  servant-event-stream-shell-with = devpkgs: haskellPackages.shellFor {
+    packages = p: [drv];
+    buildInputs = devpkgs haskellPackages;
+    withHoogle = true;
+    shellHook = ''
+        export HIE_HOOGLE_DATABASE="$(cat $(which hoogle) | sed -n -e 's|.*--database \(.*\.hoo\).*|\1|p')"
+      '';
+  };
+}
--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  shell = (import ./servant-event-stream.nix).servant-event-stream-shell-with;
+  shellWith = (import ./. {}).servant-event-stream-shell-with;
   devpkgs = p: [ p.hlint ];
 in
-  shell devpkgs
+  shellWith devpkgs
