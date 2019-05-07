21cf3f497f8b46885c904e59d23f0565ffc69c68
shaun.sharples@gmail.com <shaun.sharples@gmail.com>
2019-05-07 15:39:14 +0200

    formatting

--- src/Servant/EventStream.hs
+++ src/Servant/EventStream.hs
@@ -17,21 +17,27 @@ module Servant.EventStream
   )
 where
 
-import Control.Lens
-import Data.Binary.Builder                  (toLazyByteString)
-import qualified Data.Text                  as Text
-import qualified Data.Text.Encoding         as Text
-import GHC.Generics                         (Generic)
-import Network.HTTP.Media                   ((//), (/:))
-import Network.Wai.EventSource              (ServerEvent(..))
-import Network.Wai.EventSource.EventStream  (eventToBuilder)
+import           Control.Lens
+import           Data.Binary.Builder            ( toLazyByteString )
+import           Data.Text                      ( Text )
+import           GHC.Generics                   ( Generic )
+import           Network.HTTP.Media             ( (//)
+                                                , (/:)
+                                                )
+import           Network.Wai.EventSource        ( ServerEvent(..) )
+import           Network.Wai.EventSource.EventStream
+                                                ( eventToBuilder )
 import qualified Pipes
-import Pipes                                (X, (>->), await, yield)
-import Servant
-import Servant.Foreign
-import Servant.Foreign.Internal             (_FunctionName)
-import Servant.JS.Internal
-import Servant.Pipes                        (pipesToSourceIO)
+import           Pipes                          ( X
+                                                , (>->)
+                                                , await
+                                                , yield
+                                                )
+import           Servant
+import           Servant.Foreign
+import           Servant.Foreign.Internal       ( _FunctionName )
+import           Servant.JS.Internal
+import           Servant.Pipes                  ( pipesToSourceIO )
 
 newtype ServerSentEvents
   = ServerSentEvents (Stream Get 200 NoFraming EventStream EventSource)
@@ -39,8 +45,10 @@ newtype ServerSentEvents
 
 instance HasServer ServerSentEvents context where
   type ServerT ServerSentEvents m = ServerT (StreamGet NoFraming EventStream EventSource) m
-  route Proxy = route (Proxy :: Proxy (StreamGet NoFraming EventStream EventSource))
-  hoistServerWithContext Proxy = hoistServerWithContext (Proxy :: Proxy (StreamGet NoFraming EventStream EventSource))
+  route Proxy = route
+    (Proxy :: Proxy (StreamGet NoFraming EventStream EventSource))
+  hoistServerWithContext Proxy = hoistServerWithContext
+    (Proxy :: Proxy (StreamGet NoFraming EventStream EventSource))
 
 instance  (HasForeignType lang ftype EventSource)
   => HasForeign lang ftype ServerSentEvents where
@@ -72,25 +80,31 @@ eventSource prod = pipesToSourceIO (prod >-> yieldUntilClose)
     e <- await
     case e of
       CloseEvent -> return ()
-      _ -> yield e >> yieldUntilClose
+      _          -> yield e >> yieldUntilClose
 
-jsForAPI :: (HasForeign NoTypes NoContent api, GenerateList NoContent (Foreign NoContent api))
-         => Proxy api
-         -> Text.Text
-jsForAPI p =
-  gen (listFromAPI (Proxy :: Proxy NoTypes) (Proxy :: Proxy NoContent) p)
-   where
-    gen :: [Req NoContent] -> Text.Text
-    gen = mconcat . map genEventSource
+jsForAPI
+  :: ( HasForeign NoTypes NoContent api
+     , GenerateList NoContent (Foreign NoContent api)
+     )
+  => Proxy api
+  -> Text
+jsForAPI p = gen
+  (listFromAPI (Proxy :: Proxy NoTypes) (Proxy :: Proxy NoContent) p)
+ where
+  gen :: [Req NoContent] -> Text
+  gen = mconcat . map genEventSource
 
-    genEventSource :: Req NoContent -> Text.Text
-    genEventSource req =
-         fname <> " = function(conf)\n"
+  genEventSource :: Req NoContent -> Text
+  genEventSource req =
+    fname
+      <> " = function(conf)\n"
       <> "{\n"
-      <> " return (new EventSource(" <> url <> ", conf));\n"
+      <> " return (new EventSource("
+      <> url
+      <> ", conf));\n"
       <> "}\n"
-     where
-      fname = "var " <> (toValidFunctionName (camelCase $ req ^. reqFuncName))
-      url = if url' == "'" then "'/'" else url'
-      url' = "'" <> urlArgs
-      urlArgs = jsSegments $ req ^.. reqUrl.path.traverse
+   where
+    fname   = "var " <> (toValidFunctionName (camelCase $ req ^. reqFuncName))
+    url     = if url' == "'" then "'/'" else url'
+    url'    = "'" <> urlArgs
+    urlArgs = jsSegments $ req ^.. reqUrl . path . traverse
