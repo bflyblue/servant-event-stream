6a910606542b6f232144b5111c9fc2c9337fe141
shaun.sharples@gmail.com <shaun.sharples@gmail.com>
2019-05-09 13:45:10 +0200

    pass listeners in

--- src/Servant/EventStream.hs
+++ src/Servant/EventStream.hs
@@ -96,18 +96,22 @@ jsForAPI p = gen
   gen = mconcat . map genEventSource
 
   genEventSource :: Req NoContent -> Text
-  genEventSource req = "\n"
-    <> fname <> " = function(" <> argsStr <> ")\n"
-    <> "{\n"
-    <> "  return (new EventSource("
-    <> url
-    <> ", conf));\n"
-    <> "}\n"
+  genEventSource req = T.unlines
+    [ ""
+    , fname <> " = function(" <> argsStr <> ")"
+    , "{"
+    , "  s = new EventSource(" <> url <> ", conf);"
+    , "  eventListeners.forEach(function (ev, cb) {"
+    , "    s.addEventListener(ev, cb);"
+    , "  });"
+    , "  return s;"
+    , "}"
+    ]
    where
     argsStr = T.intercalate ", " args
     args = captures
         ++ map (view $ queryArgName . argPath) queryparams
-        ++ ["conf"]
+        ++ ["eventListeners = {}", "conf"]
 
     captures = map (view argPath . captureArg)
               . filter isCapture
