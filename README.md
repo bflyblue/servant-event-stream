df58c0f1c02665334f6addb138a1d62449da0a90
Maxim Avanov <maxim.avanov@gmail.com>
2021-04-19 17:08:40 +0100

    Some comments

---
+++ README.md
@@ -0,0 +1,21 @@
+servant-event-stream
+====================
+
+This library adds necessary type combinators to support [Server Sent Events](https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events)
+within [Servant ecosystem](https://github.com/haskell-servant/).
+
+Dev Environment
+---------------
+
+Dev env is based on [Nix](https://nixos.org) and [Niv](https://github.com/nmattia/niv).
+
+To enter the dev shell, run
+```bash
+
+nix-shell
+```
+
+You can build the project with
+```bash
+nix-build
+```
--- src/Servant/API/EventStream.hs
+++ src/Servant/API/EventStream.hs
@@ -8,7 +8,7 @@
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE UndecidableInstances       #-}
 
-module Servant.EventStream
+module Servant.API.EventStream
   ( ServerSentEvents
   , EventStream
   , EventSource
@@ -52,6 +52,7 @@ instance HasServer ServerSentEvents context where
   hoistServerWithContext Proxy = hoistServerWithContext
     (Proxy :: Proxy (StreamGet NoFraming EventStream EventSourceHdr))
 
+-- | a helper instance for <https://hackage.haskell.org/package/servant-foreign-0.15.3/docs/Servant-Foreign.html servant-foreign>
 instance  (HasForeignType lang ftype EventSourceHdr)
   => HasForeign lang ftype ServerSentEvents where
   type Foreign ftype ServerSentEvents = Req ftype
@@ -65,6 +66,9 @@ instance  (HasForeignType lang ftype EventSourceHdr)
     retType = typeFor lang (Proxy :: Proxy ftype) (Proxy :: Proxy EventSourceHdr)
     method  = reflectMethod (Proxy :: Proxy 'GET)
 
+-- | A type representation of an event stream. It's responsible for setting proper content-type
+--   and buffering headers, as well as for providing parser implementations for the streams.
+--   Read more on <https://docs.servant.dev/en/stable/tutorial/Server.html#streaming-endpoints Servant Streaming Docs>
 data EventStream
 
 instance Accept EventStream where
@@ -72,8 +76,12 @@ instance Accept EventStream where
 
 type EventSource = SourceIO ServerEvent
 
+-- | This is mostly to guide reverse-proxies like 
+--   <https://www.nginx.com/resources/wiki/start/topics/examples/x-accel/#x-accel-buffering nginx>
 type EventSourceHdr = Headers '[Header "X-Accel-Buffering" Text] EventSource
 
+-- | See details at
+--   https://hackage.haskell.org/package/wai-extra-3.1.6/docs/Network-Wai-EventSource-EventStream.html#v:eventToBuilder
 instance MimeRender EventStream ServerEvent where
   mimeRender _ = maybe "" toLazyByteString . eventToBuilder
 
