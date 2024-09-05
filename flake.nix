f1bd96cb99d26ca040bbd3d05bd08207d9727b3c
Shaun Sharples <shaun.sharples@gmail.com>
2024-09-05 16:08:49 +0200

    relax bounds

--- flake.nix
+++ flake.nix
@@ -1,42 +1,59 @@
 {
   description = "servant-event-stream";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       supportedSystems = [ "x86_64-linux" ];
-      forallSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
+      forallSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
           f (rec {
             inherit system;
             pkgs = nixpkgsFor system;
-            haskellPackages = hpkgsFor pkgs;
-          }));
+            haskellPackages = hpkgsFor system pkgs;
+          })
+        );
       nixpkgsFor = system: import nixpkgs { inherit system; };
-      hpkgsFor = pkgs:
+      hpkgsFor =
+        system: pkgs:
         with pkgs.haskell.lib;
-        pkgs.haskell.packages.ghc96.override {
-          overrides = self: super: {
-            servant-foreign = dontCheck super.servant-foreign;
+        pkgs.haskell.packages.ghc98.override { overrides = self: super: { }; };
+    in
+    {
+      packages = forallSystems (
+        {
+          system,
+          pkgs,
+          haskellPackages,
+        }:
+        {
+          servant-event-stream = haskellPackages.callCabal2nix "servant-event-stream" ./. { };
+          default = self.packages.${system}.servant-event-stream;
+        }
+      );
+      devShells = forallSystems (
+        {
+          system,
+          pkgs,
+          haskellPackages,
+        }:
+        {
+          servant-event-stream = haskellPackages.shellFor {
+            packages = p: [ self.packages.${system}.servant-event-stream ];
+            buildInputs = with haskellPackages; [
+              cabal-install
+              haskell-language-server
+            ];
+            withHoogle = true;
           };
-        };
-    in {
-      packages = forallSystems ({ system, pkgs, haskellPackages }: {
-        servant-event-stream =
-          haskellPackages.callCabal2nix "servant-event-stream" ./. { };
-        default = self.packages.${system}.servant-event-stream;
-      });
-      devShells = forallSystems ({ system, pkgs, haskellPackages }: {
-        servant-event-stream = haskellPackages.shellFor {
-          packages = p: [ self.packages.${system}.servant-event-stream ];
-          buildInputs = with haskellPackages; [
-            cabal-install
-            haskell-language-server
-          ];
-          withHoogle = true;
-        };
-        default = self.devShells.${system}.servant-event-stream;
-      });
+          default = self.devShells.${system}.servant-event-stream;
+        }
+      );
     };
 }
